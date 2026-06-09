7f1febddbe314057233b33d6be5b28b85f598b51
Duy779 <6551071016@st.utc2.edu.vn>
2026-06-09 17:29:12 +0700

    up ảnh

--- QLNH-BTL/frontend/restoran-1.0.0/js/chatbot.js
+++ QLNH-BTL/frontend/restoran-1.0.0/js/chatbot.js
@@ -1,10 +1,9 @@
-
-    (function(d, m){
-        var kommunicateSettings = 
-            {"appId":"464053e4f2d5e4d2ce921983d991c1e3","popupWidget":true,"automaticChatOpenOnNavigation":true};
-        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
-        s.src = "https://widget.kommunicate.io/kommunicate-widget-3.0.min.js";
-        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
-        window.kommunicate = m; m._globals = kommunicateSettings;
-    })(document, window.kommunicate || {});
-/* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
+ (function(d, m){
+    var kommunicateSettings =
+{"appId":"3d9bb33dfac1dd781108a9d0921082a13","popupWidget":true,"automaticChatOpenOnNavigation":true};
+    var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+    s.src = "https://widget.kommunicate.io/kommunicate-widget-3.0.min.js";
+    var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+    window.kommunicate = m; m._globals = kommunicateSettings;
+})(document, window.kommunicate || {});
+    /* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
